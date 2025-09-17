191c96e6e821b6953d2173ac5393ee22c7ccc230
LeGrizzly <xalsie.ff@hotmail.fr>
2025-09-17 14:52:49 +0200

    Refactor AuthController and AuthService: standardize field names to lowercase for consistency

--- backend/src/services/auth.service.ts
+++ backend/src/services/auth.service.ts
@@ -3,7 +3,7 @@ import { User } from '../infrastructure/airtable/models/users';
 import { env } from '../config';
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
-import Logger from '../utils/logger' // Adjust the import based on your project structure
+import Logger from '../utils/logger'
 
 const DUMMY_HASH = bcrypt.hashSync('__dummy_password_for_timing__', 10)
 
@@ -14,11 +14,9 @@ if (!JWT_SECRET) {
 
 const AuthService = {
     async login(email: string, password: string) {
-        const user = await User.getAll().then(users => users.find(u => u.Email === email)) as (TUser | null)
+        const user = await User.getAll().then(users => users.find(u => u.email === email)) as (TUser | null)
 
-        // Use a dummy hash to mitigate timing attacks
-
-        const hashToCompare = user ? user.Password : DUMMY_HASH
+        const hashToCompare = user ? user.password : DUMMY_HASH
 
         const valid = await bcrypt.compare(password, hashToCompare)
 
@@ -31,38 +29,36 @@ const AuthService = {
             role: 'role_user'
         }, JWT_SECRET, { expiresIn: env.JWT_EXPIRATION });
 
-        return { token, user: { id: user.id, email: user.Email } }
+        return { token, user: { id: user.id, email: user.email } }
     },
 
     async register({
         email,
         password,
         confirmPassword,
-        FirstName,
-        LastName,
-        School,
-        Promo,
+        firstname,
+        lastname,
+        school,
+        promo,
         telephone,
         isContacted
     }: {
         email: string,
         password: string,
         confirmPassword: string,
-        FirstName: string,
-        LastName: string,
-        School: string,
-        Promo: string,
+        firstname: string,
+        lastname: string,
+        school: string,
+        promo: string,
         telephone: string,
         isContacted: boolean
     }) {
         try {
-            // Check if email already exists
-            let existing = await User.getAll().then(users => users.find(u => u.Email === email)) as (TUser | null) // TODO : use View in Airtable to filter only local users
+            let existing = await User.getAll().then(users => users.find(u => u.email === email)) as (TUser | null) // TODO : use View in Airtable to filter only local users
 
             const hashed = await bcrypt.hash(password, 10)
 
             if (existing) {
-                // Otherwise the email is already taken by a local account
                 return { error: 'Email already in use' }
             }
 
@@ -70,35 +66,21 @@ const AuthService = {
                 return { error: 'Passwords do not match' }
             }
 
-            // Create new user
-            console.log(email,
-        password,
-        confirmPassword,
-        FirstName,
-        LastName,
-        School,
-        Promo,
-        telephone)
             const user = await User.create({
-                Email: email,
-                Password: hashed,
-                // CreatedAt: new Date().toISOString(),
-                // UpdatedAt: new Date().toISOString(),
-                // CreatedAt format four/moi/année
-                // CreatedAt: new Date().toLocaleDateString('fr-FR'),
-                // UpdatedAt: new Date().toLocaleDateString('fr-FR'),
-                isContacted: isContacted,
-                FirstName: FirstName,
-                LastName: LastName,
-                Avatar: null,
-                School: School,
-                Promo: Promo,
-                telephone: telephone,
+                [User.FieldsIds.email]: email,
+                [User.FieldsIds.password]: hashed,
+                [User.FieldsIds.isContacted]: isContacted,
+                [User.FieldsIds.firstname]: firstname,
+                [User.FieldsIds.lastname]: lastname,
+                [User.FieldsIds.avatar]: null,
+                [User.FieldsIds.school]: school,
+                [User.FieldsIds.promo]: promo,
+                [User.FieldsIds.telephone]: telephone,
             }) as TUser
 
             const token = jwt.sign({ userId: user.id, role: 'role_user' }, JWT_SECRET, { expiresIn: env.JWT_EXPIRATION })
 
-            return { token, user: { id: user.id, email: user.Email } }
+            return { token, user: { id: user.id, email: user.email } }
         } catch (error) {
             Logger.error('AuthService', 'Error in register:', error)
             throw error
